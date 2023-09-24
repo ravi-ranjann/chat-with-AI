4747c9c3c9e1623d7be3a07aac60906451cf4f23
Ravi <ravi@Ravis-MacBook-Pro.local>
2023-09-24 17:18:08 +0530

    chat gpt integration

---
+++ README.md
@@ -0,0 +1,11 @@
+# React + Vite + Chat GPT
+
+Steps to use the ChatGPT language model in this React app.
+    1. First go to https://beta.openai.com/signup/ and sign up.
+    2. Once Sign up completes, Click on Personal at top right
+    3. Then Click View API Key option.
+    4. Click on Create new secret key and generate API key.
+    5. Create an .env file
+    5. Copy the API key and paste in .env file with VITE_SOME_KEY=copied key.
+    6. npm install
+    7. npm run dev
---
+++ src/App.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import "./App.css";
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator,
+} from "@chatscope/chat-ui-kit-react";
+
+const API_KEY = import.meta.env.VITE_SOME_KEY;
+
+const App = () => {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I'm Ravi! Ask me anything!",
+      sentTime: "just now",
+      sender: "Ravi",
+    },
+  ]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  const handleSendRequest = async (message) => {
+    const newMessage = {
+      message,
+      direction: "outgoing",
+      sender: "user",
+    };
+
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setIsTyping(true);
+
+    try {
+      const response = await processMessageToChatGPT([...messages, newMessage]);
+      const content = response.choices[0]?.message?.content;
+      if (content) {
+        const chatGPTResponse = {
+          message: content,
+          sender: "Ravi",
+        };
+        setMessages((prevMessages) => [...prevMessages, chatGPTResponse]);
+      }
+    } catch (error) {
+      console.error("Error processing message:", error);
+    } finally {
+      setIsTyping(false);
+    }
+  };
+
+  async function processMessageToChatGPT(chatMessages) {
+    const apiMessages = chatMessages.map((messageObject) => {
+      const role = messageObject.sender === "Ravi" ? "assistant" : "user";
+      return { role, content: messageObject.message };
+    });
+
+    const apiRequestBody = {
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content: "I'm a professional using AI for learning",
+        },
+        ...apiMessages,
+      ],
+    };
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + API_KEY,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(apiRequestBody),
+    });
+
+    return response.json();
+  }
+
+  return (
+    <div className="">
+      <div className="App">
+        <div style={{ position: "relative", height: "700px", width: "600px" }}>
+          <MainContainer>
+            <ChatContainer>
+              <MessageList
+                scrollBehavior="smooth"
+                typingIndicator={
+                  isTyping ? (
+                    <TypingIndicator content="Ravi is typing" />
+                  ) : null
+                }
+              >
+                {messages.map((message, i) => {
+                  return <Message key={i} model={message} />;
+                })}
+              </MessageList>
+              <MessageInput
+                placeholder="Send a Message"
+                onSend={handleSendRequest}
+              />
+            </ChatContainer>
+          </MainContainer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
